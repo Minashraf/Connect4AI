61175ce7818643d221de87b8f7664b283cacf63f
pierremaged <pierremaged99@gmail.com>
2021-12-14 14:46:06 +0200

    Input window added

---
+++ Logic/Input_Window.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+depth = 0
+alpha_beta = False
+
+def set_depth():
+    global depth
+    if len(e1.get()) > 0 and e1.get().isnumeric():
+        depth = int(e1.get())
+    print(depth)
+
+def set_pruning():
+    global alpha_beta
+    alpha_beta = True
+    print(alpha_beta)
+    destroy()
+
+def destroy():
+    root.destroy()
+
+root = Tk()
+root.geometry('800x300+100+200')
+root.title('Connect 4')
+Label(root, text="Enter the depth of tree K: ").pack()
+e1 = Entry(root)
+e1.pack()
+Label(root, text='').pack()
+
+k_Button = Button(root, text='Insert K', width=13, height=1, command=set_depth)
+k_Button.pack()
+Label(root, text='').pack()
+Label(root, text='').pack()
+frame = Frame(root)
+frame.pack()
+pruning_Button = Button(frame, text='α-β pruning', width=25, height=4, fg='green', command=set_pruning)
+pruning_Button.pack(side=LEFT)
+no_pruning_Button = Button(frame, text='No pruning', width=25, height=4, fg='red', command=destroy)
+no_pruning_Button.pack(side=LEFT)
+Label(root, text='').pack()
+Label(root, text='').pack()
+f = Frame(root)
+f.pack()
+root.mainloop()
+
