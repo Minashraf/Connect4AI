96a20a64620bd08c164fe87cf3a5d0bb2b59daac
minachraf <minachraf@outlook.com>
2021-12-14 14:33:46 +0200

    Calculate score off by 1 error solved & aggregated score

--- Logic/GameEngine.py
+++ Logic/GameEngine.py
@@ -2,27 +2,30 @@ import pygame
 import time
 import random
 import Minmax
+from Calculate_score import score_calculator
 
-WIN_WIDTH   = 750
-WIN_HEIGHT  = 650
-BLACK       = (0, 0, 0)
-WHITE       = (255, 255, 255)
-RED         = (255, 0, 0)
-BLUE        = (25,25,112)
-BLUE2       = (100,149,237)
-YELLOW      = (255, 255, 0)
-BOARD       = [
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0]
-            ]
+WIN_WIDTH = 750
+WIN_HEIGHT = 650
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (25, 25, 112)
+BLUE2 = (100, 149, 237)
+YELLOW = (255, 255, 0)
+BOARD = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
 HUMAN = 1
 AGENT = 2
+human_score = agent_score = 0
 
 pygame.init()
+
 
 def main():
     global BOARD
@@ -35,7 +38,7 @@ def main():
     while run:
         draw_board(window)
 
-        #AI turn
+        # AI turn
         if not player1_turn:
             move, node = Minmax.decision(BOARD, 2, False, 0, 0)
             coordinates, score = move
@@ -47,10 +50,10 @@ def main():
                 run = False
             elif player1_turn and event.type == pygame.MOUSEBUTTONDOWN:
                 j, i = pygame.mouse.get_pos()
-                i-= 25
-                i//= 100
-                j-= 25
-                j//= 100
+                i -= 25
+                i //= 100
+                j -= 25
+                j //= 100
                 if i < 0 or j < 0 or i >= 6 or j >= 7:
                     continue
                 played = insert_tile(window, HUMAN, j)
@@ -59,30 +62,37 @@ def main():
                 else:
                     player1_turn = False
 
-#Insert tile in column, if column is full return false indicating that no move was done. Else return true
+
+# Insert tile in column, if column is full return false indicating that no move was done. Else return true
 def insert_tile(window, player, col):
-    i = 0 
-    while i<6 and BOARD[i][col] == 0:
+    i = 0
+    while i < 6 and BOARD[i][col] == 0:
         BOARD[i][col] = player
         if i > 0:
-            BOARD[i-1][col] = 0
+            BOARD[i - 1][col] = 0
         draw_board(window)
-        i+=1
+        i += 1
         time.sleep(0.05)
-    if i==0:
-        #Could not play
+    if i == 0:
+        # Could not play
         return False
     else:
-        #Played
+        # Played
+        if player == 1:
+            global human_score
+            human_score += score_calculator(BOARD, i - 1, col, player)
+        else:
+            global agent_score
+            agent_score += score_calculator(BOARD, i - 1, col, player)
         return True
-        
+
 
 def draw_board(window):
-    pygame.draw.rect(window, BLUE, pygame.Rect(25,25,700,600))
+    pygame.draw.rect(window, BLUE, pygame.Rect(25, 25, 700, 600))
     for i in range(1, 7):
-        pygame.draw.line(window, BLUE2, (25+i*100, 25),(25+i*100, 625), 3)
+        pygame.draw.line(window, BLUE2, (25 + i * 100, 25), (25 + i * 100, 625), 3)
     for i in range(1, 6):
-        pygame.draw.line(window, BLUE2, (25, 25+i*100),(725,25+i*100), 1)
+        pygame.draw.line(window, BLUE2, (25, 25 + i * 100), (725, 25 + i * 100), 1)
     for i in range(0, 6):
         for j in range(0, 7):
             if BOARD[i][j] == HUMAN:
@@ -91,8 +101,9 @@ def draw_board(window):
                 color = YELLOW
             else:
                 color = WHITE
-            pygame.draw.circle(window, color, (75+j*100, 75+i*100), 45, 0)
+            pygame.draw.circle(window, color, (75 + j * 100, 75 + i * 100), 45, 0)
     pygame.display.update()
+
 
 if __name__ == '__main__':
     main()
